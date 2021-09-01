879bcb0d9d6cd2fcd894f822f26db9b6008da078
Alexey Dolgov <bifourcator@gmail.com>
2021-09-01 12:29:22 +0300

    Initial commit

---
+++ Harvest_Project/src/main/java/harvest/HarvestRunner.java
@@ -0,0 +1,11 @@
+package harvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarvestRunner {
+	public static void main(String[] args) {
+		SpringApplication.run(HarvestRunner.class, args);
+	}
+}
