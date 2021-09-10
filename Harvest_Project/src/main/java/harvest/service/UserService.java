ca3394618d8215f2da1e96d2ab02c2030c17bf28
Alexey Dolgov <bifourcator@gmail.com>
2021-09-10 10:35:42 +0300

    Logger added

--- Harvest_Project/src/main/java/harvest/service/UserService.java
+++ Harvest_Project/src/main/java/harvest/service/UserService.java
@@ -9,6 +9,8 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,6 +26,8 @@ import harvest.domain.User;
 
 @Service
 public class UserService implements UserDetailsService {
+	Logger logger = LoggerFactory.getLogger(UserService.class);
+
 	@Autowired
 	private UserRepository userRepository;
 
@@ -38,18 +42,26 @@ public class UserService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    	logger.trace("Getting user by email \"" + email + "\" from database...");
+
 		return userRepository.findByEmail(email);
 	}
 
 	public User findById(Integer id) {
+    	logger.trace("Getting user by id=" + id + " from database...");
+
 		return userRepository.findById(id).get();
 	}
 
 	public List<User> findAll() {
+    	logger.trace("Getting all users from database...");
+
 		return userRepository.findAll();
 	}
 	
     public boolean checkIfExists(User user) {
+    	logger.trace("Checking if stored user already exists in database...");
+
     	User userFromDb = userRepository.findByEmail(user.getEmail());
 
     	if (userFromDb != null && user.getId() != userFromDb.getId()) {
@@ -59,20 +71,27 @@ public class UserService implements UserDetailsService {
     }
     
 	public boolean addUser(User user) {
+    	logger.trace("Adding new user to database...");
+
 		if (checkIfExists(user))
 			return false;
 		
-		user.setPassword(passwordEncoder.encode(user.getPassword()));
+        logger.trace("Encoding recieved user's password...");
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 		user.setActive(false);
 		user.setAccessLevels(Collections.singleton(AccessLevel.USER));
-		user.setActivationCode(UUID.randomUUID().toString());
+        logger.trace("Generating activation code...");
+        user.setActivationCode(UUID.randomUUID().toString());
 
-		userRepository.save(user);
+        logger.trace("Saving new user in database...");
+        userRepository.save(user);
 		sendActivationCode(user);
 		return true;
 	}
 
 	public void sendActivationCode(User user) {
+		logger.trace("Sending activation code to user's email...");
+
 		if (!StringUtils.isEmpty(user.getEmail())) {
 			String message = String.format("Доброго времени суток, %s %s! \n\n"
 					+ "Добро пожаловать в приложение \"Урожай©\".\n"
@@ -85,16 +104,22 @@ public class UserService implements UserDetailsService {
 	}
 
 	public boolean activateUser(String code) {
+    	logger.trace("Activating user's account...");
+
+    	logger.trace("Getting user by activation code \"" + code + "\" from database...");
 		User user = userRepository.findByActivationCode(code);
 
 		if (user == null) {
+        	logger.warn("There is no any user with activation code \"" + code + "\" in database...");
 			return false;
 		}
 
-		user.setActive(true);
+        logger.trace("Setting user's account active and clearing activation code...");
+        user.setActive(true);
 		user.setActivationCode(null);
 
-		userRepository.save(user);
+        logger.trace("Saving activated user in database...");
+        userRepository.save(user);
 
 		return true;
 	}
@@ -112,6 +137,8 @@ public class UserService implements UserDetailsService {
     }
     
     public void saveUser(User user, Map<String, String> form) {
+    	logger.trace("Updating user's account...");
+
 		user.setFirstName(form.get("firstName"));
 		user.setLastName(form.get("lastName"));
 		user.setEmail(form.get("email"));
@@ -131,13 +158,19 @@ public class UserService implements UserDetailsService {
 				user.getAccessLevels().add(AccessLevel.valueOf(key));
 			}
 		}
+		
+		logger.trace("Saving updated user in database...");
+		userRepository.save(user);
 	}
 
 	public boolean updateProfile(User user, String firstName, String lastName, String email, String password) {
+		logger.trace("Updating user's profile...");
+
 		user.setFirstName(firstName);
 		user.setLastName(lastName);
 		user.setPassword(passwordEncoder.encode(password));
 
+		logger.trace("Checking user's email for being changed...");
 		String userEmail = user.getEmail();
 		boolean isEmailChanged = (email != null && !email.equals(userEmail))
 				|| (userEmail != null && !userEmail.equals(email));
@@ -152,6 +185,7 @@ public class UserService implements UserDetailsService {
 			sendActivationCode(user);			
 		}
 
+		logger.trace("Saving updated user's profile in database...");
 		userRepository.save(user);
 		return true;
 	}
